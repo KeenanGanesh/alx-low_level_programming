1a18e6a0fa5efd305ec33c112c9161aea8f7a096
KeenanGanesh <ganeshkeenan@gmail.com>
2023-02-17 18:12:57 +0000

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
 		putchar(letter);
 	}
-		putchat('\n');
+		putchar('\n');
 
 	return (0);
 }
