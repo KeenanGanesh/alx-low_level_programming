19dc4a2e414f63cfcd719e479135625973d2e1b4
KeenanGanesh <ganeshkeenan@gmail.com>
2023-02-21 19:48:19 +0000

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print the first 98 Fibonacci numbers, starting with
+ * 1 and 2, separated by a comma followed by a space
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int a = 1, b = 2, c, i;
+
+	printf("%d, %d", a, b);
+	for (i = 3; i <= 98; i++)
+	{
+		c = a + b;
+		printf(", %d", c);
+		a = b;
+		b = c;
+	}
+
+		printf("\n");
+		return (0);
+}
+
