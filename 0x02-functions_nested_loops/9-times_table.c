032ddf11e95709dea72bffea75c8b35808418de7
KeenanGanesh <ganeshkeenan@gmail.com>
2023-02-21 17:18:26 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,24 +11,23 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-	_putchar('0');
-
+		_putchar('0');
 		for (mult = 1; mult <= 9; mult++)
 		{
-		_putchar(',');
-		_putchar(' ');
-
-		prod = num * mult;
-
-		if (prod <= 9)
+			prod = num * mult;
+			_putchar(',');
 			_putchar(' ');
+			if (prod <= 9)
+			{
+			_putchar(' ');
+			_putchar('prod + '0')
+			}
 		else
 		{
 			_putchar((prod / 10) + '0');
-
 			_putchar((prod % 10) + '0');
+		}
 		}
 		_putchar('\n');
 		}
-	}		
 }
