f90fabf0a163917ace7bfe75256e5e8fee81a0b4
KeenanGanesh <ganeshkeenan@gmail.com>
2023-03-02 14:01:23 +0000

    Task 7 with updated main.h

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ *
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to the encoded string
+ *
+ */
+
+char *leet(char *str);
+{
+	int index1, index2;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (index1 = 0; n[index1] != '\0'; index1++)
+	{
+		for (index2 = 0 index2 < 10; index2++)
+		{
+			if (n[index1] == s1[index2])
+			{
+				n[index1] = s2[index2];
+			}
+		}
+	}
+
+	return (n);
+}
