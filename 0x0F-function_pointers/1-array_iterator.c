7b03f5fa3f83df4f7a01a685811d79c7fec03c73
KeenanGanesh <ganeshkeenan@gmail.com>
2023-03-22 14:00:39 +0000

    corrected error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter 
+ * array_iterator - executes a function given as a parameter0
  *	            on each element of an array
  * @array: the array
  * @size: size of the array
