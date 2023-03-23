09d2285733235d687d06529f6ad7736a8422c63c
KeenanGanesh <ganeshkeenan@gmail.com>
2023-03-23 10:31:50 +0000

    0-sum_them_all.c + test files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#include <stdagr.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 /**
  * struct dt - struct
@@ -13,10 +18,5 @@ typedef struct dt
 	char letter;
 	void (*func)(va_list);
 } datatype;
-
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
 #endif
